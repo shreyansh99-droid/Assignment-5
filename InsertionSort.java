df8f46f0519897f253265845017384e60cb72ae4
shreyansh99-droid <shreyanshsingh105@gmail.com>
2025-04-26 19:21:25 +0530

    Create InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,48 @@
+//Write a Java program to implement Insertion Sort.
+
+public class InsertionSort {
+    // Method to perform insertion sort
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            // Move elements of array[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    // Method to print the array
+    public static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        int[] arr = { 9, 5, 1, 4, 3 };
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
+
+
+//OUTPUT 
+Original array:
+9 5 1 4 3 
+Sorted array:
+1 3 4 5 9
